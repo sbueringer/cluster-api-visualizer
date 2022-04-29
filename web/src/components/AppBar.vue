472acf0d8a9875307b05def5de1916619a9b5c66
Jonathan Tong <jt572@cornell.edu>
2022-04-29 17:54:26 -0400

    Add menu bar component

--- web/src/components/AppBar.vue
+++ web/src/components/AppBar.vue
@@ -25,6 +25,8 @@
       v-else
     ></v-app-bar-nav-icon>
     <v-toolbar-title class="text-no-wrap pa-0">{{ title }}</v-toolbar-title>
+
+    <v-spacer></v-spacer>
     <v-tooltip bottom>
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -43,59 +45,73 @@
       <span>Reload resources</span>
     </v-tooltip>
 
-    <v-spacer></v-spacer>
-    <v-tooltip bottom>
+    <v-menu
+      offset-y
+      :close-on-content-click="false"
+    >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
           icon
-          text
-          class="ma-2"
-          @click="$emit('zoomOut', true)"
           v-bind="attrs"
           v-on="on"
         >
-          <v-icon color="white">
-            {{"mdi-minus"}}
-          </v-icon>
+          <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
       </template>
-      <span>Zoom out</span>
-    </v-tooltip>
-    <span>{{ Math.round(scale*100) }}%</span>
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          icon
-          text
-          class="ma-2"
-          @click="$emit('zoomIn', true)"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon color="white">
-            {{"mdi-plus"}}
-          </v-icon>
-        </v-btn>
-      </template>
-      <span>Zoom in</span>
-    </v-tooltip>
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          icon
-          text
-          class="ma-2"
-          @click="$emit('togglePathStyle', !isStraight);"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon color="white">
-            {{ isStraight ? 'mdi-sine-wave' : 'mdi-square-wave' }}
-          </v-icon>
-        </v-btn>
-      </template>
-      <span>Toggle link style</span>
-    </v-tooltip>
+
+      <v-list min-width="300px">
+
+        <v-list-item @click="$emit('togglePathStyle', !isStraight);">
+          <v-list-item-icon class="menu-icon">
+            <v-icon>{{ isStraight ? 'mdi-sine-wave' : 'mdi-square-wave' }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Toggle link style</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item class="list-item">
+          <v-list-item-icon class="menu-icon">
+            <v-icon>mdi-magnify</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <div class="wrap">
+              <v-list-item-title class="menu-title">Zoom</v-list-item-title>
+              <v-btn
+                icon
+                text
+                class="d-inline-block mx-1"
+                small
+                @click="$emit('zoomOut', true)"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>
+                  {{"mdi-minus"}}
+                </v-icon>
+              </v-btn>
+              <span>{{Math.round(scale*100) }}%</span>
+
+              <v-btn
+                icon
+                text
+                class="d-inline-block mx-1"
+                small
+                @click="$emit('zoomIn', true)"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>
+                  {{"mdi-plus"}}
+                </v-icon>
+              </v-btn>
+            </div>
+
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-menu>
 
   </v-app-bar>
 </template>
@@ -115,6 +131,26 @@ export default {
 <style lang="less" scoped>
 #appBar {
   z-index: 2000;
+}
+
+.wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  // background-color: aqua;
+}
+
+.list-item {
+  // background-color: red;
+}
+
+.menu-icon {
+  // background-color: indigo;
+}
+
+.menu-title {
+  display: inline-block;
 }
 
 .router-link {
